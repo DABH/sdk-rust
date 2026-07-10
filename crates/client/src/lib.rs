957d455c09f14c6d57cf445bb87b8f1c12dc054b
jmaeagle99 <44687433+jmaeagle99@users.noreply.github.com>
2026-07-10 12:48:59 -0700

    fix: comment

--- crates/client/src/lib.rs
+++ crates/client/src/lib.rs
@@ -130,7 +130,7 @@ static TEMPORAL_NAMESPACE_HEADER_KEY: &str = "temporal-namespace";
 /// Key used to communicate when a GRPC message is too large
 pub static MESSAGE_TOO_LARGE_KEY: &str = "message-too-large";
 #[doc(hidden)]
-/// The violation, if `status` is the client proactively rejecting an outbound request for exceeding a
+/// Returns the violation, if `status` is the client proactively rejecting an outbound request for exceeding a
 /// payload/memo error size limit.
 pub fn payload_limit_violation_from(
     status: &tonic::Status,
